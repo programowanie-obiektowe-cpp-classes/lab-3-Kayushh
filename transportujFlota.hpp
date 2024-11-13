ecdcbc4e02515b020e731552fd3dd6198bec41cd
Kayushhh <czesiuleczek4@gmail.com>
2024-11-13 15:19:00 +0100

    Zejsciowka3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,18 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0) return 0;
+    Stocznia stocznia;
+    unsigned int tot_towar = 0;
+    unsigned int ile_zaglowcow = 0;
+
+    while (tot_towar < towar) {
+        Statek* statek = stocznia(); 
+        tot_towar += statek->transportuj(); 
+        if (dynamic_cast<Zaglowiec*>(statek)) {
+            ++ile_zaglowcow;
+        }
+        delete statek; 
+    }
+    return ile_zaglowcow; 
 }
